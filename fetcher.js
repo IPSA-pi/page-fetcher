743a17c6e848a925596535908954f9541f73fa1e
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-17 16:13:21 -0700

    dont use net module, use request package to get response from url

--- fetcher.js
+++ fetcher.js
@@ -1,35 +1,29 @@
-const net = require('net');
 const fs = require('fs');
-
+const request = require('request');
 const { argv } = require('process');
 
 const args = argv.slice(2);
-const URL = args[0];
+const url = args[0];
 const localPath = args[1];
 
-const conn = net.createConnection({
-  host: URL,
-  port: 80,
-});
-
-conn.on('connect', () => {
-  console.log('Connected to site');    
-  conn.write(`GET / HTTP/1.1\r\n`);
-  conn.write(`Host: ${URL}\r\n`);
-  conn.write(`\r\n`);
-});
-conn.on('data', (data) => {
-  // console.log(data);
-  const body = data.substring(data.indexOf('<'));
-  fs.appendFile(localPath, body, err => {
-    if (err) {
-      console.log(err);
+const fetchAndSave = function (url, localPath) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log("Failed to download resource: ", error);
+      return;
     }
+    // Optional: it's okay if students don't bother checking the http response code for this exercise.
+    fs.writeFile(localPath, body, (error) => {
+      if (error) {
+        console.log("Failed to write to localPath: ", localPath);
+      } else {
+        console.log(
+          `Downloaded and saved ${body.length} bytes to ${localPath}`
+        );
+      }
+    });
   });
-  console.log(body);
-  console.log(`Downloaded and saved ${body.length} bytes to ${localPath}.`)
-  conn.end();
-});
+};
 
-conn.setEncoding('UTF8');
+fetchAndSave(url, localPath);
 
